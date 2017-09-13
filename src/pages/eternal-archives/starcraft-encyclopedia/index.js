42e35f65a45b7beabbade9be0b1a75b1be30b843
Georgi Yanev <georgiyanev.gy@gmail.com>
2017-09-14 01:19:04 +0300

    Add StarCraft screenshots, SIE and DarkRevenge

--- src/pages/eternal-archives/starcraft-encyclopedia/index.js
+++ src/pages/eternal-archives/starcraft-encyclopedia/index.js
@@ -22,7 +22,8 @@ const StarCraftEncyclopediaPage = () =>
 
     <div className="m-b-1">
       <p>To say I had my fair share of StarCraft obsession doesn't even begin to describe it. I also wrote an encyclopedia on the topic, covering every unit of the game. Excuse the comic sans, and pictures created in MS Paint :)</p>
-      <p>The Encyclopedia also came as a website and an executable for Windows, coded in C++.</p>
+      <p>The StarCraft Interactive Encyclopedia (SIE) also came as a website and an executable for Windows, coded in C++ - <a href={__PATH_PREFIX__ + '/SIE.exe'}>SIE.</a></p>
+      <p>Also, here's a bonus custom campaign I made: <a href={__PATH_PREFIX__ + '/DarkRevenge--byZasz.rar'}>Dark Revenge</a></p>
     </div>
 
     <iframe src={__PATH_PREFIX__ + '/starcraft-encyclopedia-by-georgi.pdf'} width="800px" height="2100px" />
--- src/pages/eternal-archives/starcraft-screenshots/index.js
+++ src/pages/eternal-archives/starcraft-screenshots/index.js
@@ -2,7 +2,15 @@ import React from 'react'
 import Link from 'gatsby-link'
 import Helmet from 'react-helmet'
 
-import starcraftBugs from '../../../images/starcraft-bugs.png'
+import starcraftBug2 from '../../../images/starcraft-bug2.png'
+import starcraftBug3 from '../../../images/starcraft-bug3.png'
+import starcraftBug4 from '../../../images/starcraft-bug4.png'
+import starcraftBug5 from '../../../images/starcraft-bug5.png'
+import starcraftBug6 from '../../../images/starcraft-bug6.png'
+import starcraftBug7 from '../../../images/starcraft-bug7.png'
+import starcraftBug8 from '../../../images/starcraft-bug8.png'
+import starcraftBug9 from '../../../images/starcraft-bug9.png'
+import starcraftBug10 from '../../../images/starcraft-bug10.png'
 
 const StarCraftScreenShotsPage = () =>
   <div>
@@ -23,7 +31,40 @@ const StarCraftScreenShotsPage = () =>
     </div>
 
     <div className="m-b-1">
-      <img src={starcraftBugs} alt="Some funny StarCraft bugs" />
+      <img src={starcraftBug2} alt="Nexus with a Nuclear Silo" />
+      <p>Nexus with a Nuclear Silo</p>
+    </div>
+    <div className="m-b-1">
+      <img src={starcraftBug3} alt="Infested Spore Colony" />
+      <p>Infested Spore Colony</p>
+    </div>
+    <div className="m-b-1">
+      <img src={starcraftBug4} alt="Invisible minerals moving" />
+      <p>Invisible minerals moving</p>
+    </div>
+    <div className="m-b-1">
+      <img src={starcraftBug5} alt="A Creep Colony builds another Creep Colony?" />
+      <p>A Creep Colony builds another Creep Colony?</p>
+    </div>
+    <div className="m-b-1">
+      <img src={starcraftBug6} alt="Showing through the stones" />
+      <p>Showing through the stones</p>
+    </div>
+    <div className="m-b-1">
+      <img src={starcraftBug7} alt="Reavers' ammo holds still since it can't quite make it" />
+      <p>Reavers' ammo holds still since it can't quite make it</p>
+    </div>
+    <div className="m-b-1">
+      <img src={starcraftBug8} alt="Infested Spore Colony" />
+      <p>Infested Spore Colony</p>
+    </div>
+    <div className="m-b-1">
+      <img src={starcraftBug9} alt="Zergling in the middle of a Hatchery" />
+      <p>Zergling in the middle of a Hatchery</p>
+    </div>
+    <div className="m-b-1">
+      <img src={starcraftBug10} alt="That exact moment when the Bunker is destroyed" />
+      <p>That exact moment when the Bunker is destroyed</p>
     </div>
 
     <Link to="/">Go back to the homepage</Link>
