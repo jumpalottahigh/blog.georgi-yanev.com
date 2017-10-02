1207654a1f08b3d03696f8bf11673720022cec2d
Georgi Yanev <georgiyanev.gy@gmail.com>
2017-10-02 23:26:11 +0300

    Generate dynamic page lists using GraphQL

--- src/pages/eternal-archives/index.js
+++ src/pages/eternal-archives/index.js
@@ -1,11 +1,38 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
-const EternalArchivesPage = () =>
+const EternalArchivesPage = ({data}) =>
   <div>
     <h1>Eternal Archives - home to the past...</h1>
     <p>This section hosts uneditted things the way they were in the late 90s and early 2000s.</p>
+    <p>List of all posts in this section:</p>
+    <ul className="list-none">
+      {data.allSitePage.edges.map(page => {
+          if (page.node.path.includes('eternal-archives/')) {
+            return (
+              <li>
+                <Link key={page.node.id} to={page.node.path} rel="noopener">{page.node.path}</Link>
+              </li>
+            )
+          }
+        }
+      )}
+    </ul>
     <Link to="/">Go back to the homepage</Link>
   </div>
 
 export default EternalArchivesPage
+
+// Export dynamic list of all pages
+export const pageQuery = graphql`
+  query IndexQuery {
+    allSitePage(limit: 1000) {
+      edges {
+        node {
+          id
+          path
+        }
+      }
+    }
+  }
+`
--- src/pages/news/index.js
+++ src/pages/news/index.js
@@ -1,11 +1,38 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
-const NewsPage = () =>
+const NewsPage = ({data}) =>
   <div>
     <h1>News and articles</h1>
     <p>All newest articles end up in this section. You can expect content on JavaScript and Web Dev, FPV drones, Smart Home Automation with Home Assistant and more.</p>
+    <p>List of all posts in this section:</p>
+    <ul className="list-none">
+      {data.allSitePage.edges.map(page => {
+          if (page.node.path.includes('news/')) {
+            return (
+              <li>
+                <Link key={page.node.id} to={page.node.path} rel="noopener">{page.node.path}</Link>
+              </li>
+            )
+          }
+        }
+      )}
+    </ul>
     <Link to="/">Go back to the homepage</Link>
   </div>
 
 export default NewsPage
+
+// Export dynamic list of all pages
+export const pageQuery2 = graphql`
+  query IndexQuery2 {
+    allSitePage(limit: 1000) {
+      edges {
+        node {
+          id
+          path
+        }
+      }
+    }
+  }
+`
