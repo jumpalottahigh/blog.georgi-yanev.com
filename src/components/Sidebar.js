00c869f7e28e816d3bf0cf3ecf9456a353f2cb35
Georgi Yanev <georgiyanev.gy@gmail.com>
2017-12-07 21:57:49 +0200

    Fix feedback buttons mac styles; Move menu emoji

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -7,7 +7,7 @@ const SideBar = () => (
       <h5>Blog posts</h5>
       <ul className="sidenav">
         <li>
-          <em>Smart home automation</em> 🏠
+          🏠 <em>Smart home automation</em>
         </li>
         <li>
           <Link to="/smart-home/living-with-smart-home-automation">
@@ -15,7 +15,7 @@ const SideBar = () => (
           </Link>
         </li>
         <li>
-          <em>Software development</em> 💻
+          💻 <em>Software development</em>
         </li>
         <li>
           <Link to="/software/how-to-become-a-good-developer">
@@ -23,7 +23,7 @@ const SideBar = () => (
           </Link>
         </li>
         <li>
-          <em>FPV quads</em> 🚁
+          🚁 <em>FPV quads</em>
         </li>
         <li>
           <Link to="/fpv/getting-started-with-fpv-drones/">
