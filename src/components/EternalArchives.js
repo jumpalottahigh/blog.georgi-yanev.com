e92ada39f60585deb8481107b2ae8b21db111e98
Georgi Yanev <georgiyanev.gy@gmail.com>
2018-05-10 15:51:12 +0300

    Create Sidebar toggle-able component

---
+++ src/components/EternalArchives.js
@@ -0,0 +1,142 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+import svgRightArrow from '../images/right-arrow.svg'
+
+// TODO: Pass real data as props to this component from a GraphQL query
+
+export default class EternalArchives extends React.Component {
+  state = {
+    isOpen: false
+  }
+
+  toggleOpen = () => {
+    this.setState(prevState => ({
+      isOpen: !prevState.isOpen
+    }))
+  }
+
+  render() {
+    return (
+      <div>
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            cursor: 'pointer'
+          }}
+          onClick={this.toggleOpen}
+        >
+          <h5>
+            Eternal Archives
+            <br />
+            <span>/1999-2003/</span>
+          </h5>
+          <img
+            src={svgRightArrow}
+            style={{
+              height: '30px',
+              transition: '475ms',
+              transform: `rotate(${this.state.isOpen ? 90 : 0}deg)`
+            }}
+          />
+        </div>
+        {this.state.isOpen && (
+          <ul className="sidenav">
+            <li>
+              <em>Fan Fiction</em>
+            </li>
+            <li>
+              <Link to="/eternal-archives/fiction/leaders-of-the-great-dark-beyond/">
+                Leaders of the Great Dark Beyond
+                <span className="sidenav-info">/a Warcraft story/</span>
+              </Link>
+            </li>
+            <li>
+              <Link to="/eternal-archives/fiction/sargeras-tomb/">
+                Sargeras' tomb
+                <span className="sidenav-info">/a Warcraft story/</span>
+              </Link>
+            </li>
+            <li>
+              <Link to="/eternal-archives/fiction/triumph-of-the-orcish-hordes/">
+                Triumph of the orcish hordes
+                <span className="sidenav-info">/a Warcraft story/</span>
+              </Link>
+            </li>
+            <li>
+              <Link to="/eternal-archives/fiction/the-small-village-tavern/">
+                The small village tavern
+                <span className="sidenav-info">/a Warcraft story/</span>
+              </Link>
+            </li>
+            <li>
+              <Link to="/eternal-archives/fiction/athene-d2-character-story/">
+                Athine Palade:
+                <span className="sidenav-info">/Diablo 2 character story/</span>
+              </Link>
+            </li>
+            <li>
+              <Link to="/eternal-archives/fiction/log-of-gelus-life/">
+                Log of Gelu's life
+                <span className="sidenav-info">
+                  /a Heroes of Might and Magic story/
+                </span>
+              </Link>
+            </li>
+            <li>
+              <em>Media</em>
+            </li>
+            <li>
+              <Link to="/eternal-archives/music/">Music</Link>
+            </li>
+            <li>
+              <Link to="/eternal-archives/music-videos-and-mixtapes/">
+                Videos and Mixtapes
+              </Link>
+            </li>
+            <li>
+              <Link to="/eternal-archives/game-dev-videos/">
+                Game Dev Videos
+              </Link>
+            </li>
+            <li>
+              <em>Random</em>
+            </li>
+            <li>
+              <Link to="/eternal-archives/starcraft-encyclopedia/">
+                StarCraft Encyclopedia
+              </Link>
+            </li>
+            <li>
+              <Link to="/eternal-archives/starcraft-screenshots/">
+                StarCraft Screenshots
+              </Link>
+            </li>
+            <li>
+              <Link to="/eternal-archives/diablo-unique-mobs/">
+                Diablo Unique Mobs
+              </Link>
+            </li>
+            <li>
+              <Link to="/eternal-archives/warcraft-units/">WarCraft Units</Link>
+            </li>
+            <li>
+              <Link to="/eternal-archives/funny-win98-bugs/">
+                Funny Windows 98 bugs
+              </Link>
+            </li>
+            <li>
+              <Link to="/eternal-archives/batch-files/">Batch files</Link>
+            </li>
+            <li>
+              <Link to="/eternal-archives/my-first-site-and-programs/">
+                My first site and programs
+              </Link>
+            </li>
+          </ul>
+        )}
+      </div>
+    )
+  }
+}
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import EternalArchives from '../components/EternalArchives'
 
 const activeStyle = {
   color: '#0057e7',
   fontWeight: 600,
   fontSize: '0.95rem'
 }
-// TODO: Break this into static eternal archives component and graphql generated article list
+
 export default class SideBar extends React.Component {
   constructor(props) {
     super(props)
@@ -108,102 +109,7 @@ export default class SideBar extends React.Component {
             <li className="coming-soon">how to install betaflight 3.2</li>
           </ul>
           <hr />
-          <h5>
-            Eternal Archives <span>/1999-2003/</span>
-          </h5>
-          <ul className="sidenav">
-            <li>
-              <em>Fan Fiction</em>
-            </li>
-            <li>
-              <Link to="/eternal-archives/fiction/leaders-of-the-great-dark-beyond/">
-                Leaders of the Great Dark Beyond
-                <span className="sidenav-info">/a Warcraft story/</span>
-              </Link>
-            </li>
-            <li>
-              <Link to="/eternal-archives/fiction/sargeras-tomb/">
-                Sargeras' tomb
-                <span className="sidenav-info">/a Warcraft story/</span>
-              </Link>
-            </li>
-            <li>
-              <Link to="/eternal-archives/fiction/triumph-of-the-orcish-hordes/">
-                Triumph of the orcish hordes
-                <span className="sidenav-info">/a Warcraft story/</span>
-              </Link>
-            </li>
-            <li>
-              <Link to="/eternal-archives/fiction/the-small-village-tavern/">
-                The small village tavern
-                <span className="sidenav-info">/a Warcraft story/</span>
-              </Link>
-            </li>
-            <li>
-              <Link to="/eternal-archives/fiction/athene-d2-character-story/">
-                Athine Palade:
-                <span className="sidenav-info">/Diablo 2 character story/</span>
-              </Link>
-            </li>
-            <li>
-              <Link to="/eternal-archives/fiction/log-of-gelus-life/">
-                Log of Gelu's life
-                <span className="sidenav-info">
-                  /a Heroes of Might and Magic story/
-                </span>
-              </Link>
-            </li>
-            <li>
-              <em>Media</em>
-            </li>
-            <li>
-              <Link to="/eternal-archives/music/">Music</Link>
-            </li>
-            <li>
-              <Link to="/eternal-archives/music-videos-and-mixtapes/">
-                Videos and Mixtapes
-              </Link>
-            </li>
-            <li>
-              <Link to="/eternal-archives/game-dev-videos/">
-                Game Dev Videos
-              </Link>
-            </li>
-            <li>
-              <em>Random</em>
-            </li>
-            <li>
-              <Link to="/eternal-archives/starcraft-encyclopedia/">
-                StarCraft Encyclopedia
-              </Link>
-            </li>
-            <li>
-              <Link to="/eternal-archives/starcraft-screenshots/">
-                StarCraft Screenshots
-              </Link>
-            </li>
-            <li>
-              <Link to="/eternal-archives/diablo-unique-mobs/">
-                Diablo Unique Mobs
-              </Link>
-            </li>
-            <li>
-              <Link to="/eternal-archives/warcraft-units/">WarCraft Units</Link>
-            </li>
-            <li>
-              <Link to="/eternal-archives/funny-win98-bugs/">
-                Funny Windows 98 bugs
-              </Link>
-            </li>
-            <li>
-              <Link to="/eternal-archives/batch-files/">Batch files</Link>
-            </li>
-            <li>
-              <Link to="/eternal-archives/my-first-site-and-programs/">
-                My first site and programs
-              </Link>
-            </li>
-          </ul>
+          <EternalArchives />
         </div>
       </div>
     )
