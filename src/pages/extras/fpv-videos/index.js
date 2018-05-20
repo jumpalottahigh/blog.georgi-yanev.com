ba88d658c96680a8bbc0d6940f2c71fe84d21a1d
Georgi Yanev <georgiyanev.gy@gmail.com>
2018-05-20 23:37:11 +0300

    Add 2 more FPV videos to listing

--- src/pages/extras/fpv-videos/index.js
+++ src/pages/extras/fpv-videos/index.js
@@ -13,6 +13,8 @@ const Grid = styled.div`
 
 const FPVVideos = () => {
   const videoURLsHD = [
+    'https://www.youtube.com/embed/wNNG2Iwh46U',
+    'https://www.youtube.com/embed/qRN9jqY8jv0',
     'https://www.youtube.com/embed/37F2ckku7uU',
     'https://www.youtube.com/embed/uyWB6Cu0klc',
     'https://www.youtube.com/embed/398BDc5mvuA',
