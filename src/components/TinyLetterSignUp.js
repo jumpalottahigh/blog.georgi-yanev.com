5d132c75ca59cc95e3f6ff343b7aa35b21ccb89d
Georgi Yanev <georgiyanev.gy@gmail.com>
2019-04-28 22:26:48 +0300

    Arrange copy

--- src/components/TinyLetterSignUp.js
+++ src/components/TinyLetterSignUp.js
@@ -102,9 +102,8 @@ const TinyLetterSignup = () => {
     <TinyLetter>
       <h3>📰 Subscribe to the newsletter</h3>
       <p className="one">
-        Get the latest updates straight to your inbox.
-        <br />
-        Email frequency - 1/2 times a month.
+        Get the latest updates straight to your inbox. Email frequency - 1/2
+        times a month.
       </p>
       <form
         action="https://tinyletter.com/jumpalottahigh"
