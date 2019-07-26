c78867715ddf2461ee2e7e32e4a711e9e3097564
Georgi Yanev <georgiyanev.gy@gmail.com>
2019-07-26 23:53:22 +0300

    Make Share component to use icons, improve copy

--- src/components/Share.js
+++ src/components/Share.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import { FaFacebookSquare, FaTwitterSquare } from 'react-icons/fa'
 
 const StyledShare = styled.section`
   padding: 1rem 0;
@@ -7,32 +8,39 @@ const StyledShare = styled.section`
 
   h4 {
     margin: 0;
+    display: flex;
+    align-items: center;
   }
 
   a {
-    margin-right: 1rem;
+    margin-left: 0.5rem;
+    box-shadow: none !important;
+  }
+
+  a:hover {
+    opacity: 0.85;
   }
 `
 
 const Share = ({ url }) => (
   <StyledShare>
-    <h4>Share this article on:</h4>
-    <div>
+    <h4>
+      Share this on social media:{' '}
       <a
         href={`https://www.facebook.com/sharer/sharer.php?u=https://blog.georgi-yanev.com${url}`}
         target="_blank"
         rel="noreferrer"
       >
-        Facebook
+        <FaFacebookSquare size="1.75rem" color="#4267b2" />
       </a>
       <a
-        href={`https://twitter.com/home?status=https://blog.georgi-yanev.com${url}`}
+        href={`https://twitter.com/intent/tweet?text=https://blog.georgi-yanev.com${url}`}
         target="_blank"
         rel="noreferrer"
       >
-        Twitter
+        <FaTwitterSquare size="1.75rem" color="#1da1f2" />
       </a>
-    </div>
+    </h4>
   </StyledShare>
 )
 
