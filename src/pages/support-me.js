b868e3d2825a97d658291b13a73f24ecebdf0a65
jumpalottahigh <georgiyanev.gy@gmail.com>
2020-02-02 22:26:39 +0200

    Create support-me page

---
+++ src/pages/support-me.js
@@ -0,0 +1,168 @@
+import React from 'react'
+import { Link } from 'gatsby'
+import styled from 'styled-components'
+import Layout from '../components/structure/layout'
+
+const Section = styled.section`
+  text-align: center;
+`
+
+const Task = styled.div`
+  padding-top: 1rem;
+  text-align: left;
+  border-bottom: 1px solid #dedede;
+
+  h2 {
+    font-size: 1.2rem;
+    line-height: 1.4rem;
+    margin-bottom: 0.8rem;
+  }
+
+  p {
+    margin-top: 0.4rem;
+    line-height: 1.2rem;
+    font-size: 0.9rem;
+  }
+`
+
+const SupportMePage = () => (
+  <Layout>
+    <Section>
+      <h1>Support me</h1>
+      <h5>If you like what I do and would like to help me do more of it...</h5>
+      <Task>
+        <h2>
+          1.{' '}
+          <a
+            href="https://www.youtube.com/channel/UCCh3SK2EktDdOQkEOTDmSCg"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Subscribe to FPVtips
+          </a>{' '}
+          on YouTube
+        </h2>
+        <p>
+          Costs you nothing, helps much more than you may realise and is very
+          appreciated! Bonus points for <strong>liking</strong> videos, watching
+          from start to finish and leaving{' '}
+          <strong>feedback in the comments</strong>!
+        </p>
+      </Task>
+      <Task>
+        <h2>
+          2.{' '}
+          <a
+            href="https://www.banggood.com/?p=NY211410857261201705"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Click my affiliate link
+          </a>{' '}
+          before purchasing anything from Banggood
+        </h2>
+        <p>
+          It doesn't cost you anything, and I will get a small commision. It all
+          adds up, and I re-invest almost everything back into more FPV gear and
+          drones for reviews, builds and mods which I then publish on the blog
+          and on the YouTube channel.
+        </p>
+      </Task>
+      <Task>
+        <h2>3. One time or a recurring donation</h2>
+        <p>
+          If you'd like to toss a few bucks my way, you can do so via{' '}
+          <a
+            className="paypal"
+            style={{ fontSize: '1rem' }}
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://paypal.me/jumpalottahigh/10"
+          >
+            PayPal
+          </a>{' '}
+          or{' '}
+          <a
+            className="patreon"
+            style={{ fontSize: '1rem' }}
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://www.patreon.com/bePatron?u=3061524"
+          >
+            Patreon
+          </a>
+          . I'd like to also take a moment to <strong>THANK</strong> everyone
+          who has decided to do this so far! It means a lot to me that you
+          consider the content I put out worth your money! Thank you!
+        </p>
+      </Task>
+      <Task>
+        <h2>
+          4. <Link to="/newsletter/">Signup</Link> for the monthly newsletter
+        </h2>
+        <p>
+          No spam. I send out up to 1 email per month and can skip less eventful
+          months.
+        </p>
+      </Task>
+      <Task>
+        <h2>
+          5.{' '}
+          <a
+            href="https://www.youtube.com/channel/UC2gwYMcfb0Oz_fl9W1uTV2Q"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Subscribe to Georgi FPV
+          </a>{' '}
+          on YouTube
+        </h2>
+        <p>
+          I don't put nearly as much effort into my second channel, it's mostly
+          for dumping raw flight footage of various drones. But there are some
+          pretty nice freestyle videos in there.
+        </p>
+      </Task>
+      <Task>
+        <h2>6. Follow me on social media on YouTube</h2>
+        <p>
+          I upload video snippets, behind the scenes, random flight footage,
+          gear pictures every so often on:{' '}
+          <a
+            href="https://www.instagram.com/fpvtips"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Instagram
+          </a>
+          ,{' '}
+          <a
+            href="https://twitter.com/fpvtips"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Twitter
+          </a>
+          ,{' '}
+          <a
+            href="https://facebook.com/fpvtips"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Facebook
+          </a>
+        </p>
+      </Task>
+      <Task>
+        <h2>7. Keep enjoying FPV! Happy flying!</h2>
+      </Task>
+    </Section>
+    <h2
+      style={{ textAlign: 'center', marginTop: '2rem', marginBottom: '3rem' }}
+    >
+      Thank you!
+    </h2>
+  </Layout>
+)
+
+export default SupportMePage
