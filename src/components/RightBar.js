5a751c23ebc2bdafda63e6b97831ed1b1fe8ee31
jumpalottahigh <georgiyanev.gy@gmail.com>
2020-05-20 16:17:56 +0300

    Try out some sidebar ideas

--- src/components/RightBar.js
+++ src/components/RightBar.js
@@ -1,28 +1,49 @@
 import React from 'react'
 import styled from 'styled-components'
+import { Link } from 'gatsby'
+
+import TagPool from './TagPool'
 
 const StyledRightBar = styled.div`
-  /* a {
-    color: #0175d8;
-    text-decoration: none;
-    box-shadow: none;
-  }
-
-  a:hover {
-    color: #0175d8;
-    box-shadow: none;
-  } */
-
   display: none;
 
   @media (min-width: 1550px) {
     display: block;
     width: 400px;
+    padding: 1rem;
+
+    .container {
+      border-left: 1px solid #cecece;
+      padding: 1rem;
+      min-height: 240px;
+    }
+
+    .container.main-links {
+      a {
+        color: #0175d8;
+      }
+
+      a:hover {
+        font-weight: 600;
+      }
+    }
   }
 `
 
 const RightBar = () => {
-  return <StyledRightBar>Hi</StyledRightBar>
+  return (
+    <StyledRightBar>
+      <div className="container main-links">
+        <h4>Reviews &amp; Setup guides: </h4>
+        <a href="#">Test</a>
+        <h4>How to's &amp; DIY's: </h4>
+        <Link to="/fpv/">Setup EmuFlight &amp; Project Mockingbird</Link>
+      </div>
+      <div className="container">
+        <TagPool />
+      </div>
+    </StyledRightBar>
+  )
 }
 
 export default RightBar
