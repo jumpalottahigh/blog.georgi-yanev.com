bebf99aa1d88d11aa702f0fb02fd36e30dec047f
Georgi Yanev <georgiyanev@MacBook-Pro.home>
2020-05-28 11:28:57 +0300

    Upgrade deps

--- src/templates/fpv-news.js
+++ src/templates/fpv-news.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { graphql, Link } from 'gatsby'
-import Helmet from 'react-helmet'
+import { Helmet } from 'react-helmet'
 import Layout from '../components/structure/layout'
 import FeedbackSection from '../components/FeedbackSection'
 import AskAQuestion from '../components/AskAQuestion'
